a06eb0912a91562db701cd71edd342b4dc3bb821
Alex <alexrogersdesign@gmail.com>
2021-11-11 08:06:02 -0500

    Cleaned up top bar components

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -6,28 +6,17 @@ import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import {AppBarProps} from '@material-ui/core';
 
-import {makeStyles, Theme, createStyles} from '@material-ui/core/styles';
-
 import {ChildrenProps} from '../shared/types';
 import {RestContext} from '../context/rest/RestContext';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    cancelIcon: {
-      fill: 'red',
-      color: '#f44336',
-    },
-  }),
-);
 interface Props extends ChildrenProps{
   className?: string,
   position?: AppBarProps['position'],
 }
 
 const TopBar = ({children, className, position}: Props) => {
-  const classes = useStyles();
-
   const {currentUser} = useContext(RestContext);
+
   return (
     <AppBar className={className} position={position? position: 'static'}>
       <Toolbar>

002118bd4c934922987feac4ad95ba950ee55268
Alex <alexrogersdesign@gmail.com>
2021-12-25 11:41:05 -0500

    Cleanup repo

--- backend/src/websocket.d.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from "socket.io";
-import { DefaultEventsMap } from "socket.io/dist/typed-events";
-/**
- * The websocket setup function. Creates socket listeners that implement
- * the SocketIO functionality.
- * @param {Server} io The SocketIO server
- */
-declare const websocket: (io: Server<DefaultEventsMap, DefaultEventsMap>) => void;
-export default websocket;

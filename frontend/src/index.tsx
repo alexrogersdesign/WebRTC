99481a4d2d35f78e6b38ca946d97ae4919b07b1a
Alex <alexrogersdesign@gmail.com>
2021-12-28 12:09:27 -0500

    Cleanup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,11 +10,7 @@ const dsn = process.env.REACT_APP_SENTRY_DSN;
 Sentry.init({
   dsn: dsn,
   integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.8,
 });
 
 if (process.env.NODE_ENV === 'production') {
